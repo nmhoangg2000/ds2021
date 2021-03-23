4edd5c0916fd1b188d6d4d867f2b596861d13034
longtranthanh19 <longtranthanh6@gmail.com>
2021-03-23 17:15:26 +0700

    Update UI

---
+++ Final-Project/client/src/components/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="chat-room-container">
+            <div className="row">
+                <div className="col-4">
+                <h1 className="online-name">Online: </h1>
+                <div className="online-container">
+                    <ol className="online-list">
+                    
+                    </ol>
+                </div>
+                </div>
+                <div className="col-8">
+                    <h1 className="room-name">Room: </h1>
+                    <div className="messages-container">
+                        <ol className="messages-list">
+                        
+                        </ol>
+                    </div>
+                    <div className="row">
+                        <textarea
+                            placeholder="Write message..."
+                            className="new-message-input-field"
+                        />
+                        <button className="send-message-button">
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
