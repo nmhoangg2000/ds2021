1dfdfec28c43e2f8358babcd8038f1c7e070d814
longtranthanh19 <longtranthanh6@gmail.com>
2021-03-23 17:17:06 +0700

    UI Change

--- Final-Project/client/src/components/Home/Home.jsx
+++ Final-Project/client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="chat-room-container">
             <div className="row">
-                <div className="col-4">
+                <div className="col-3">
                 <h1 className="online-name">Online: </h1>
                 <div className="online-container">
                     <ol className="online-list">
@@ -13,7 +13,7 @@ const Home = () => {
                     </ol>
                 </div>
                 </div>
-                <div className="col-8">
+                <div className="col-9">
                     <h1 className="room-name">Room: </h1>
                     <div className="messages-container">
                         <ol className="messages-list">
