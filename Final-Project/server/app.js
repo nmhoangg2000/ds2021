9b8373256ccbc283851e588e8b43fd4763d42f38
Thanh Ha Ng Le <harley.ngx@gmail.com>
2021-03-23 17:22:09 +0700

    Init Socket.io

--- Final-Project/server/app.js
+++ Final-Project/server/app.js
@@ -7,7 +7,13 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const port = process.env.PORT || 3000;
-app.listen(port, (err) => {
+
+const server = app.listen(port, (err) => {
     if (err) console.log(err);
     else console.log(`Server listen on port ${port}`);
+})
+
+const io = require('./socket').init(server);
+io.on('connection', socket => {
+
 })
